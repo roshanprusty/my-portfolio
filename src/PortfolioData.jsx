2f174c14145fcbaae0fdbfa0ac5b4f3929d057fd
roshan <roshanprusty100@gmail.com>
2023-01-31 21:48:15 +0530

    Version3

---
+++ src/PortfolioData.jsx
@@ -0,0 +1,61 @@
+import proj1 from "images/fyp.png";
+import proj2 from "images/netflix.PNG";
+import proj3 from "images/stripe.PNG";
+import proj4 from "images/covid.png";
+import proj5 from "images/groc_bud.PNG";
+import proj6 from "images/color.PNG";
+
+
+const PortfolioData = [
+  {
+    id: 1,
+    title: "Job Finder",
+    tagline: "Easy to find Jobs",
+    liveUrl: "https://job-finder-rnp.netlify.app/",
+    repositoryUrl: "https://github.com/roshanprusty/Job-Finder",
+  },
+  {
+    id: 2,
+    title: "Hotstar clone",
+    tagline: "Hotstar Clone with React JS",
+    liveUrl: "https://m-junaid95.github.io/Netflix-Clone/",
+    repositoryUrl: "https://github.com/roshanprusty/Hotstar-clone",
+    image: "https://soyacincau.com/wp-content/uploads/2021/06/210601-disney-plus-hotstar-malaysia-hero-02.jpg",
+  },
+  {
+    id: 3,
+    title: "IMDB",
+    tagline: "Imdb Clone with React JS",
+    liveUrl: "https://m-junaid95.github.io/Stripe-Menu/",
+    repositoryUrl: "https://github.com/m-https://github.com/roshanprusty/IMDB/Stripe-Menu",
+    image: "https://cdn.discordapp.com/attachments/780837848030380112/929812133288493086/cover_image.png",
+  },
+
+  {
+    id: 4,
+    title: "Airbnb Experince",
+    tagline: "Airbnb Experince with React JS",
+    liveUrl: "https://m-junaid95.github.io/Reactjs-Covid19-Tracker/",
+    repositoryUrl: "https://github.com/m-https://github.com/roshanprusty/Airbnb-Experince/Reactjs-Covid19-Tracker",
+    image: "https://www.tom.travel/wp-content/uploads/2021/03/Airbnb-Experiences.jpg",
+  },
+  
+  // {
+  //   id: 5,
+  //   title: "GROCERY-BUD-APP",
+  //   tagline: "TO DO LIST with ReactJs Also Used Local Storage",
+  //   liveUrl: "https://m-junaid95.github.io/Grocery-Bud-React-js/",
+  //   repositoryUrl: "https://github.com/m-junaid95/Grocery-Bud-React-js",
+  //   image: proj5,
+  // },
+  
+  // {
+  //   id: 6,
+  //   title: "Color-Generator-App-with-ReactJs",
+  //   tagline: "Randomly Color Picked",
+  //   liveUrl: "https://m-junaid95.github.io/Color-Generator/",
+  //   repositoryUrl: "https://github.com/m-junaid95/Color-Generator",
+  //   image: proj6,
+  // },
+];
+export default PortfolioData;
